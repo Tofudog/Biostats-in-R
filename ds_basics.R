7070f6417fddc9bc50742cdd7e72523aa87d1e0d
leode <leodefarias25@gmail.com>
2022-07-01 17:34:07 -0500

    added first preliminary tutorial

--- ds_basics.R
+++ ds_basics.R
@@ -21,3 +21,27 @@ cats <- c("cat1", "cat2", "cat3")
 
 #better way to generate cats
 cats <- paste("cat", 1:10, sep="")
+
+#naming elements
+birthdays <- c("July", "August", "November", "January")
+people = c("Mary", "Jeff", "Jonah", "Lily")
+names(birthdays) <- people
+
+#truncating or abbreviating
+STATES <- c("Arkansas", "California", "Mississippi", "Louisiana",
+			"Missouri", "New York")
+print(substr(STATES, 1, 3))  # problematic for "New York", abbreviating to "New"
+print(abbreviate(STATES, minlength=3))  # better because not just first 3 chars
+
+#Categorical vectors: for discreet levels
+DISEASE <- c("Smallpox", "Influenza", "Covid", "Diabetes", Covid", "Influenza")
+DISEASE <- factor(DISEASE)  # now it's not character vector
+
+#matricies
+tic_tac_toe <- matrix(rep(0, 9), nrow=3)
+
+#dataset
+salary <- c(10.2, 62.5, 38.2, 91.3, 66.4, 105.2, 49.8, 56.6)
+emotion <- factor(c("happy", "bored", "sad", "sad", "happy",
+				"sad", "happy", "bored"))
+DATAFRAME <- data.frame(salary, emotion)
